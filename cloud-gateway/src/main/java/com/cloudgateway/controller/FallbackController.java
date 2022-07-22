18d89cf70dec00fdaafa3ff6d57b1225d0ed6d1e
MANISH <ms7644030@gmail.com>
2022-07-22 19:37:43 +0530

    API gateway

---
+++ cloud-gateway/src/main/java/com/cloudgateway/controller/FallbackController.java
@@ -0,0 +1,26 @@
+package com.cloudgateway.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+
+import reactor.core.publisher.Mono;
+
+@RestController
+public class FallbackController {
+	
+	//@HystrixCommand(fallbackMethod = "pensionerDetailFallBack")
+	@RequestMapping("/pdFallBack")
+	public Mono<String> pensionerDetailFallBack(){
+		
+		return Mono.just("Pensioner Detail Service is taking too long to respond or is down. Please try again later");
+	}
+	
+	@RequestMapping("/ppFallBack")
+	public Mono<String> processPensionFallBack(){
+		
+		return Mono.just("Process Pension Service is taking too long to respond or is down. Please try again later");
+	}
+
+}
